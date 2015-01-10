0b7e941b89866aa67d1e86652d9c29952ae80104
Nils <nils@wireklint.se>
2015-01-10 23:29:26 +0100

    added url input on cli and clipboard

--- streampy.py
+++ streampy.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python3
 from __future__ import division
 from subprocess import Popen, PIPE
 import pyperclip as pc
 import sys
 
+verbose=False
+
 def read_url_from_clipboard():
     clipboard = pc.paste()
-    print( type(clipboard))
     clip_string = clipboard.encode('UTF-8','ignore')
-    print(clip_string)
     return clip_string
+
+def conditional_print(arguments):
+    if verbose:
+        print(arguments)
 
 def main():
     arguments = sys.argv
@@ -17,49 +22,65 @@ def main():
     else:
         url = read_url_from_clipboard()
 
-    try play_url(url)
+    try:
+        play_url(url)
+    except Exception as e:
+        print(e)
+        pass
 
 def play_url(url):
-    read_url_from_clipboard()
-    url = 'http://www.twitch.tv/forgg'
-    url = 'https://www.youtube.com/watch?v=QcqC4jtrR9Y'#regular
-    url = 'https://www.youtube.com/watch?v=NJ3aiM8K6D0'#protected
     livestreamerargs = ['best']
+    test = ['a', 'b']
+    youtubeviewerargs = ['best', '--no-interactive']
+    print(type(url))
+    #if isinstance(url, unicode):
+    #    url = url.encode('UTF-8','ignore')
+    if isinstance(url, bytes):
+        url = url.decode('UTF-8')
     if 'twitch.tv' in url:
-        print('twitch stream')
-        args = ['livestreamer', url] + livestreamerargs
-        print(args, len(args))
-        Popen(args)
-    elif 'youtube.com' not in url:
+        if verbose:
+            print('twitch stream')
 
-        print('not youtube url - possibly shortened')
+        args = ['livestreamer', url] + livestreamerargs
+        if verbose:
+            print(args, len(args))
+        Popen(args)
+
+    elif 'youtube.com' not in url:
+        if verbose:
+            print('not youtube url - possibly shortened')
 
     if 'youtube.com' in url:
-        print('youtube url')
+        if verbose:
+            print('youtube url')
         args = ['livestreamer', url] + livestreamerargs
-        print(args, len(args))
+        if verbose:
+            print(args, len(args))
+
         p = Popen(args, stdout=PIPE, stderr=PIPE)
         outmsg, errmsg = p.communicate()
 
-        print("process output\n", outmsg)
-        print("error?", errmsg)
-        print("msg type", type(outmsg))
+        if verbose:
+            print("process output\n", outmsg)
+            print("error?", errmsg)
+            print("msg type", type(outmsg))
+
         outmsg_string = outmsg.decode(encoding='UTF-8')
         if 'youtube-dl' in outmsg_string:
-            print("process output\n", outmsg)
-            print("TRYING youtube-viewer")
-            args[0] = 'youtube-viewer'
-            args.append('--no-interactive')
+            if verbose:
+                print("process output\n", outmsg)
+                print("TRYING youtube-viewer")
+
+            #args[0] = 'youtube-viewer'
+            #args.append('--no-interactive')
+            args = ['youtube-viewer', url] + youtubeviewerargs
             p = Popen(args, stdout=PIPE, stderr=PIPE)
-            m = p.stdout
             outmsg = p.communicate()
 
-            """
-            outmsg_string = outmsg.decode(encoding='UTF-8')
-            print(outmsg_string)
-            if 'Select' in outmsg_string:
-                #don't want to select anothe video to watch, kill process
-                None
-                p.term()
-            """
 if __name__ == '__main__':
+    main()
+    """
+    url = 'http://www.twitch.tv/forgg'
+    url = 'https://www.youtube.com/watch?v=QcqC4jtrR9Y'#regular
+    url = 'https://www.youtube.com/watch?v=NJ3aiM8K6D0'#protected
+    """
